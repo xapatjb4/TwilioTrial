86e245291fc179aa8e53cd7fae6fb2e6d28ff6cb
Patrick <xa.patjb4@gmail.com>
2019-10-17 21:08:38 -0400

    tested twilio, letting go for presentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TwilioTrial
 Trying twillio for UMass presentation
+Not using anymore because trial account doesn't allow replies to unverified numbers
+Was able to test it using the ngork script to expose the port I was running the server on.
--- index.js
+++ index.js
@@ -3,12 +3,23 @@ dotenv.config();
 const express = require('express')
 const app = express();
 const twilio = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 
 
 app.get('/', (req, res) => {
   res.send(process.env.TWILIO_SID)
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('hi there');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+app.listen(80, () => {
+  console.log('Example app listening on port 80!')
 });
